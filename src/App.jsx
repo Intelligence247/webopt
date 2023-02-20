ad798638df5a2ee357d1acf8f2be036e143e110d
piouskenny <kehindeu13@gmail.com>
2023-02-20 05:39:21 +0100

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Logo from "./assets/images/logo.png";
 function App() {
   return (
-    <div className="text-center  flex justify-center items-center  bg-gradient-to-r from-purple-500 to-blue-500 ">
-     
-     <h1 className="text-6xl text-gray-200 my-12 p-12">WEB-OPT</h1>
-      
+    <div className="text-center  flex justify-center items-center  bg-gradient-to-r from-purple-100 to-white ">
+      <div>
+        <img src={Logo} alt="Logo" />
+
+        <h1 className="text-xl text-purple-500 text-center">
+          Increae Visibility Through Web Optimization
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
