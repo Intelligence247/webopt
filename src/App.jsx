a86704f2b66626f58a7cb3eed8c96a83120a2d8c
piouskenny <kehindeu13@gmail.com>
2023-03-02 19:12:44 +0100

    Deleted Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Faqs from "./components/Faqs";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
 import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 import Packages from "./components/Packages";
 import Steps from "./components/Steps";
 import WhatTodo from "./components/WhatTodo";
+import Faqs from "./components/Faqs";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="body">
