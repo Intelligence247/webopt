69aae1a2940bd9198dee8413292e9916bf36abed
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-03-02 13:40:03 +0100

    packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
+import Packages from "./components/Packages";
+import Steps from "./components/Steps";
 function App() {
   return (
-    <div className="">
+    <div className="body">
       <Nav/>
       <Hero/>
+      <Steps/>
+      <Packages/>
     </div>
   );
 }
---
+++ src/components/Step1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Step1 = () => {
+  return (
+    <div className='flex items-center gap-4'>
+        <img src="/media/react.svg" alt="" />
+        <p className='leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus officiis temporibus provident, autem alias voluptate labore commodi ipsum ducimus</p>
+
+    </div>
+  )
+}
+
+export default Step1
