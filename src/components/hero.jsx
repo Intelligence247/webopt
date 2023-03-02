1321e71312ca389606895f236b13b622dcbc4a85
piouskenny <kehindeu13@gmail.com>
2023-03-02 15:03:57 +0100

    Edited the Hero Section Name

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import "./Hero.css"
+import React from "react";
+import "./Hero.css";
 const Hero = () => {
   return (
-    <div className='wraperhero'>
-        <h1>Maximize Your Business Potential With Web-Opt</h1>
-    <p>Welcome to Web-Opt, where we help businesses like yours improve their online visibility and attract more customers. We're committed to help you achieve your goals and grow your business</p>
-    <button>Get Started</button>
+    <div className="wraperhero">
+      <h1>Maximize Your Business Potential With Web-Opt</h1>
+      <p>
+        Welcome to Web-Opt, where we help businesses like yours improve their
+        online visibility and attract more customers. We're committed to help
+        you achieve your goals and grow your business
+      </p>
+      <button>Get Started</button>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
