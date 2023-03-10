9959ae9569ad3a058392a13fe2f4cbcbb2b0f235
piouskenny <kehindeu13@gmail.com>
2023-03-10 07:44:51 +0100

    Added the steps for using webopt

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
-import Step1 from './Step1'
+import React from 'react';
+import Step1 from './steps/Step1';
+import Step2 from './Steps/Step2';
+import Step3
+
+from './Steps/Step3';
 import "./Step.css"
 const Steps = () => {
   return (
@@ -8,8 +12,8 @@ const Steps = () => {
        <div className="visibility">
         <h1>Boost Your Online Visibility in just a few steps</h1>
         <Step1/>
-        <Step1/>
-        <Step1/>
+        <Step2/>
+        <Step3/>
        </div>
     </div>
   )
---
+++ src/components/steps/Step3.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Step3 = () => {
+  return (
+    <div className="flex items-center gap-4">
+      <img
+        src="/media/arrow_right.svg"
+        alt=""
+        className="w-8 h-8 md:w-10 md:h-10"
+      />
+      <p className="leading-6 lg:text-base text-sm text-gray-700">
+        <span className="font-bold text-black block md:inline-block">
+          Promote your website on social media:
+        </span>{" "}
+        Our team can help you create and manage social media profiles for your
+        business on popular platforms like Facebook, Twitter, and Instagram. We
+        can also create social media posts and ads to help promote your website
+        and increase your views. By reaching a wider audience on social media,
+        you can improve your online visibility and attract more customers to
+        your website.
+      </p>
+    </div>
+  );
+};
+
+export default Step3;
