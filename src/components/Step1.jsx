f25cadc821b1fd34cb8fc86fd31cd47f4575d47e
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-03-20 22:46:57 +0100

    fixed:responsiveness for Iphones

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Step1 = ({title, desc}) => {
   return (
-    <div className='flex items-center gap-4'>
+    <div className='flex items-center space-x-4'>
         <img src="/media/check_circle.png" alt="" className="w-8 h-8 md:w-10 md:h-10" />
         <p className='leading-6 lg:text-base text-sm text-gray-700'><span className="font-bold text-black block md:inline-block mr-1"> {title} </span> {desc}</p>
 
